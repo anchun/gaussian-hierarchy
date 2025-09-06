6dbc2a49e0a3006fe5c09a37157478ac8cbefb96
anchun <nkterry@163.com>
2025-09-06 20:01:18 +0800

    export sky for ply

---
+++ build/CMakeFiles/GaussianHierarchyPlyGenerator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaussianHierarchyPlyGenerator.dir/mainHierarchyPlyGenerator.cpp.o"
+  "CMakeFiles/GaussianHierarchyPlyGenerator.dir/mainHierarchyPlyGenerator.cpp.o.d"
+  "GaussianHierarchyPlyGenerator"
+  "GaussianHierarchyPlyGenerator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaussianHierarchyPlyGenerator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GaussianHierarchyPlyGenerator.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GaussianHierarchyPlyGenerator.
+# This may be replaced when dependencies are built.
